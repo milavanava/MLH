ce6e149bc9a85e20c29e99fc5eb66b7e9626da6e
unknown <milavanava@gmail.com>
2020-11-05 19:57:57 -0500

    add data and helpers 2

--- test/smoke/mainFunctionality.js
+++ test/smoke/mainFunctionality.js
@@ -1,15 +1,27 @@
 const sel = require ('../../data/selectors.json');
+const data = require ('../../data/testData.json');
+const inputValues4 = require ('../../helpers/inputValues4.js');
+const inputValues4andClick = require ('../../helpers/inputValues4andClick.js');
+
 describe('Checking the main functionality', function () {
+
     describe('Happy path', function () {
+
         it('TC-021 Create button is clickable after 1-4 are filled in', function () {
             browser.url('');
-            const inputName = $(sel.name).setValue("LadyBug");
-            const inputGender = $$(sel.gender)[1].click();
-            const inputAge = $(sel.age).setValue(123);
-            const click = $(sel.story).click();
-            const inputStory = $$(sel.storyType)[6].click();
+            inputValues4(data.name, data.gender.she, data.age, data.storyType);
             const create = $(sel.create).isEnabled();
             expect(create).toEqual(true);
+        });
+    });
+
+    describe('Other paths', function () {
+
+        it('TC-022 gender he is working', function () {
+            browser.url('');
+            inputValues4andClick(data.name, data.gender.he, data.age, data.storyType);
+            const btn = $(sel.tryAgain).isDisplayed();
+            expect(btn).toEqual(true);
         });
     });
 });
